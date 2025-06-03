1639900d96f53441a459c241ab2d566b23129809
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-06-03 11:24:05 +0200

    setup the first routes, updates later

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :agency, only: [:show] do
+    resources :connection, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+      resources :listing, only: [:show, :update]
+      resources :connection_comment, only: [:index, new, :create]
+    end
+    resources :listing, only: [:index, :new]
+  end
 end
