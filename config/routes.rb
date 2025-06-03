a11ba30dfabb8e35825c856447c28828a51f9ffd
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-06-03 13:32:25 +0200

    bugfix for routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :agency, only: [:show] do
-    resources :listing, only: [:index, :new, :create]
-    resources :travel, only: [:index]
-    resources :hotel, only: [:index]
+  resources :agencies, only: [:show] do
+    resources :listings, only: [:index, :new, :create]
+    resources :travels, only: [:index]
+    resources :hotels, only: [:index]
   end
 end
