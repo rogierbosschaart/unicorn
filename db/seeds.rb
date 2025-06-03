10b8d701782c9aaa78f7995e6d969ddddd815d17
Tymur Topchu <timur.han98@gmail.com>
2025-06-03 15:11:29 +0200

    add delete travel for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+Travel.destroy_all
+Hotel.destroy_all
 
-Agency.destroy_all
 User.destroy_all
+Agency.destroy_all
+
 agent = User.create!(f_name: "Rogier", l_name: "Bosschaart", username: "rbosschaart", user_type: "agent", email: "rogierbosschaart@gmail.com", password: "123456")
 model = User.create!(f_name: "Tymur", l_name: "Topchu", username: 'tymur', user_type: 'mannequin', email: "tymur@gmail.com", password: "123456")
 success = Agency.create!( name: "Success", city: "Paris" )
@@ -11,7 +14,6 @@ Travel.create!( origin: "Amsterdam", destination: "Paris", date_time: "03/06/202
 Travel.create!( origin: "Paris", destination: "Milan", date_time: "03/06/2026 23:48", user: User.mannequins.last, agency: Agency.last )
 Travel.create!( origin: "Milan", destination: "New-York", date_time: "04/06/2026 12:32", user: User.mannequins.last, agency: Agency.last )
 
-Hotel.destroy_all
 Hotel.create!( name: "Hotel Zeeuws-Meisje", address: "Hoofdpoortstraat 2A, 4301 AM Zierikzee, Netherlands", extra_info: "This cozy boutique hotel is located in the center of Zierikzee", user: User.last, agency: Agency.last )
 Hotel.create!( name: "Pension De Appelgaard", address: "Culemborg, Gelderland, Netherlands", extra_info: "This charming guesthouse offers clean and quiet rooms, a delightful breakfast, and a relaxing atmosphere." , user: User.last, agency: Agency.last)
 Hotel.create!( name: "Palau Pacific Resort", address: "Koror, Republic of Palau 96940", extra_info: "A luxurious resort hotel set on 64 acres of tropical forest, offering ideal conditions for a peaceful getaway.", user: User.last, agency: Agency.last )
