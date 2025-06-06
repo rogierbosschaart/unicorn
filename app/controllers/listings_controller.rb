579e3c92c1d672f51cba664bc3223da3df5d97cf
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-06-06 16:17:20 +0200

    we can now see the info inside listings in the dashboard

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -5,8 +5,8 @@ class ListingsController < ApplicationController
   end
 
   def show
-    @agency = Agency.find(params[:agency_id])
     @listing = Listing.find(params[:id])
+    @connections = @listing.connections
   end
 
   def new
@@ -41,6 +41,7 @@ class ListingsController < ApplicationController
 
   def create_connections_from_session
     selected_model_ids = session[:selected_model_ids] || []
+    selected_models = ModelAgencyProfile.where(id: selected_model_ids)
 
     selected_models.each do |model|
       Connection.create!(
