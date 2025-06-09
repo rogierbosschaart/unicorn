e014ba177581374633145b2df2150e1867e788a7
Tymur Topchu <timur.han98@gmail.com>
2025-06-09 15:58:06 +0200

    markers works

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -5,6 +5,7 @@ export default class extends Controller {
     apiKey: String,
     markers: Array
   }
+
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
     this.map = new mapboxgl.Map({
@@ -12,7 +13,6 @@ export default class extends Controller {
       style: "mapbox://styles/mapbox/streets-v10"
     })
     // Add zoom controls
-    this.map.addControl(new mapboxgl.NavigationControl())
     // Disable scroll zoom to prevent zooming with mousepad/mouse wheel
     // this.map.scrollZoom.disable()
     // Disable double click zoom
@@ -20,27 +20,24 @@ export default class extends Controller {
     // Disable touch zoom for mobile devices
     // this.map.touchZoomRotate.disable()
     this.#addMarkersToMap()
-    // this.#fitMapToMarkers()
+    this.#fitMapToMarkers()
   }
+
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
       const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
 
-      // Create a HTML element for your custom marker
-      const customMarker = document.createElement("div")
-      customMarker.innerHTML = marker.marker_html
-
       // Pass the element as an argument to the new marker
-      new mapboxgl.Marker(customMarker)
+      new mapboxgl.Marker(marker)
         .setLngLat([marker.lng, marker.lat])
         .setPopup(popup)
         .addTo(this.map)
     })
   }
 
-  // #fitMapToMarkers() {
-  //   const bounds = new mapboxgl.LngLatBounds()
-  //   this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
-  //   this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-  // }
+   #fitMapToMarkers() {
+     const bounds = new mapboxgl.LngLatBounds()
+     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
+     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
 }
---
+++ vendor/javascript/@mapbox--mapbox-sdk.js
@@ -0,0 +1,4 @@
+// @mapbox/mapbox-sdk@0.13.7 downloaded from https://ga.jspm.io/npm:@mapbox/mapbox-sdk@0.13.7/index.js
+
+import r from"#lib/client.js";var a={};var t=r;a=t;var e=a;export{e as default};
+
