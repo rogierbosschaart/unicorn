ddb60264ec7742556a0d3705d8db699f913297b1
Tymur Topchu <timur.han98@gmail.com>
2025-06-10 17:40:21 +0200

    cale

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,22 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+import flatpickr from "flatpickr"
+
+document.addEventListener("turbo:load", () => {
+  flatpickr(".date-picker", {
+    locale: "en", // или просто не указывать — английский по умолчанию
+    dateFormat: "Y-m-d",
+    allowInput: true,
+  });
+
+  flatpickr(".month-picker", {
+    locale: "en",
+    dateFormat: "Y-m",
+    plugins: [new monthSelectPlugin({
+      shorthand: true,
+      dateFormat: "Y-m",
+      altFormat: "F Y",
+    })],
+  });
+});
