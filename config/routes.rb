aeb168decd7e316ff2ffbea43562c1c347c3eb6a
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-06-10 13:06:37 +0200

    almost done wioth basic style of connection show

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,8 @@ Rails.application.routes.draw do
 
   # AGENCY
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
-  resources :connections, only: [:new, :create, :show] do
-    resources :connection_comments, only: [:create]
-  end
   resources :clients, only: [:new, :create]
   resources :posts, only: [:index, :new, :create]
-
   resources :model_agency_profiles, only: [:new, :create, :update, :edit]
   resources :agencies do
     resources :listings, only: [:index, :show, :new, :create]
@@ -21,6 +17,11 @@ Rails.application.routes.draw do
   get 'home', to: 'model_agency_profiles#home', as: :home
   get 'inbox', to: 'model_agency_profiles#inbox', as: :inbox
   get 'travel', to: 'model_agency_profiles#travel', as: :travel
+
+  # EVERYONE
+  resources :connections, only: [:new, :create, :show] do
+    resources :connection_comments, only: [:new, :create]
+  end
 
   # MAP
   get 'map', to: 'pages#map', as: :map
