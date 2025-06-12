523ddc8b2e9a56791b6ebfa051bf2ca442410865
Rogier Bosschaart <rogierbosschaart@gmail.com>
2025-06-12 17:12:47 +0200

    removed extra import

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,5 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-import ListingModalController from "./listing_modal_controller"
-application.register("listing-modal", ListingModalController)
